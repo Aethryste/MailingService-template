966380bdd49ee2faeb84dc4183df4596f45b328c
Richard Algra <richard.b.algra@gmail.com>
2024-03-03 17:47:52 +0100

    Finish email service, now fully functional

--- src/main/java/com/mailingService/classes/EmailRequest.java
+++ src/main/java/com/mailingService/classes/EmailRequest.java
@@ -21,24 +21,12 @@ public class EmailRequest {
         return to;
     }
 
-    public void setTo(String to) {
-        this.to = to;
-    }
-
     public String getSubject() {
         return subject;
     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
     public String getBody() {
         return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 
     @Override
--- src/main/java/com/mailingService/services/EmailService.java
+++ src/main/java/com/mailingService/services/EmailService.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 import java.util.Objects;
 
 @Service
@@ -17,10 +20,29 @@ public class EmailService {
     private Environment env;
 
     public void sendEmail(String to, String subject, String body) {
+        JavaMailSenderImpl mailSender = (JavaMailSenderImpl) javaMailSender;
+
+        Session session = Session.getInstance(mailSender.getJavaMailProperties(),
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(
+                                env.getProperty("spring.mail.username"),
+                                env.getProperty("spring.mail.password")
+                        );
+                    }
+                });
+
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(Objects.requireNonNull(env.getProperty("spring.mail.username")));
+        message.setFrom(Objects.requireNonNull(env.getProperty("spring.mail.username")));
+        message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
-        javaMailSender.send(message);
+
+        try {
+            javaMailSender.send(message);
+            System.out.println("Email sent successfully!");
+        } catch (Exception ex) {
+            System.err.println("Failed to send email: " + ex.getMessage());
+        }
     }
 }
