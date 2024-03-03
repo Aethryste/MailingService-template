9852aa0e2da0a9fe40ccccca93f8f9ad6d18f2f9
Richard Algra <richard.b.algra@gmail.com>
2024-03-03 14:30:03 +0100

    Initial commit

---
+++ src/main/java/com/mailingService/controllers/EmailController.java
@@ -0,0 +1,21 @@
+package com.mailingService.controllers;
+
+import com.mailingService.classes.EmailRequest;
+import com.mailingService.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/sendEmail")
+    public String sendEmail(@RequestBody EmailRequest emailRequest) {
+        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getBody());
+        return "Email sent successfully!";
+    }
+}
